5e6180da6c589dab1526db77654b7fd77eb97af5
rrweller <riley.r.weller@gmail.com>
2023-10-01 14:58:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,6 @@ pip3 install yt_dlp
 pip3 install requests
 
 # Replace this line with the relevant torch version from this site https://pytorch.org/get-started/locally/
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118/torch_nightly.html
+pip3 install torch torchvision torchaudio
 
 echo "All necessary libraries have been installed!"
